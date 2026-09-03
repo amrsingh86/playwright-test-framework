5e7e0c3c033b708d4a18a0d67b37aff985b855af
CodeWithHashman <hashmanjot.kaur@gmail.com>
2026-09-03 05:39:13 -0400

    Initialize playwright typescript framework

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './tests',
+
+    fullyParallel: true,
+
+    use: {
+        baseURL: 'https://www.saucedemo.com',
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure'
+    },
+
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] }
+        }
+    ],
+
+    reporter: [
+        ['html'],
+        ['list']
+    ]
+});
